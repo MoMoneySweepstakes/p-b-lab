6343bae0c03d4411b87bffee855cac79a8d2fb02
MoMoneySweepstakes <mo5470860@gmail.com>
2025-11-17 04:02:46 -0500

    Add script to create Proxmox agents

--- p-b-lab/proxmox/create_agents.sh
+++ p-b-lab/proxmox/create_agents.sh
@@ -1,2 +1,37 @@
 
 
+
+#!/bin/bash
+
+# --- SETTINGS ---
+AGENT_NAME=$1
+
+if [ -z "$AGENT_NAME" ]; then
+  echo "Usage: ./create_agent.sh <agent-name>"
+  exit 1
+fi
+
+# VM/LXC template settings
+TEMPLATE_ID=9000
+NEW_ID=$((200 + RANDOM % 500))
+
+# Download if template not found
+if ! qm status $TEMPLATE_ID &> /dev/null; then
+  echo "Downloading Debian template..."
+  pveam update
+  pveam download local debian-12-standard_12.0-1_amd64.tar.zst
+  TEMPLATE_ID=local:vztmpl/debian-12-standard_12.0-1_amd64.tar.zst
+fi
+
+# Create container
+echo "Creating container..."
+pct create $NEW_ID $TEMPLATE_ID \
+  -hostname $AGENT_NAME \
+  -cores 1 \
+  -memory 1024 \
+  -net0 name=eth0,bridge=vmbr0,ip=dhcp,type=veth
+
+# Start the agent
+pct start $NEW_ID
+
+echo "$AGENT_NAME created with ID $NEW_ID"
